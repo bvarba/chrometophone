9a926915159b623149234c34cc4b5741c8624808
burke.davey <burke.davey@dad15e60-07c4-0504-eb71-dc8a1c21358d>
2010-08-04 15:59:42 +0000

    Fix regex bug

--- extension/content_script.js
+++ extension/content_script.js
@@ -21,7 +21,8 @@ var pageInfo = {
 };
 
 // URL overrides
-if (pageInfo.url.match(/^http[s]?:\/\/maps\.google\./)) {
+if (pageInfo.url.match(/^http[s]?:\/\/maps\.google\./) ||
+    pageInfo.url.match(/^http[s]?:\/\/www\.google\.[a-z]{2,3}(\.[a-z]{2})\/maps/)) {
   var link = document.getElementById('link');
   if (link && link.href) {
     pageInfo.url = link.href;
