b581e46278dd6ba03eb606e4dd86f45f4b08e142
burke.davey <burke.davey@dad15e60-07c4-0504-eb71-dc8a1c21358d>
2010-08-30 20:38:22 +0000

    Remove superfluous import

--- appengine/src/com/google/android/chrometophone/server/BrowserChannelServlet.java
+++ appengine/src/com/google/android/chrometophone/server/BrowserChannelServlet.java
@@ -17,7 +17,6 @@
 package com.google.android.chrometophone.server;
 
 import java.io.IOException;
-import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
